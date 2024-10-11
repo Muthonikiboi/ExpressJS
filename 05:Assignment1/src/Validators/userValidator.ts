f6abaf93ea282fe794a830ebf19a6d86c79da403
Muthonikiboi <kiboijoye254@gmail.com>
2024-10-11 14:47:35 +0300

    ADD:Seperation of Concerns

---
+++ 05:Assignment1/src/Validators/userValidator.ts
@@ -0,0 +1,32 @@
+import { body, param } from "express-validator";
+
+export const createUserValidator = [
+  body("UserID").notEmpty().withMessage("UserID is required"),
+  body("FirstName").notEmpty().withMessage("First Name is required"),
+  body("LastName").notEmpty().withMessage("Last Name is required"),
+  body("Email").isEmail().withMessage("Invalid email format"),
+  body("Phone").isMobilePhone("any").withMessage("Invalid phone number"),
+  body("Country").notEmpty().withMessage("Country is required"),
+];
+
+export const updateUserValidator = [
+  param("id").notEmpty().withMessage("UserID is required"),
+  body("FirstName").optional().notEmpty().withMessage("First Name is required"),
+  body("LastName").optional().notEmpty().withMessage("Last Name is required"),
+  body("Email").optional().isEmail().withMessage("Invalid email format"),
+  body("Phone").optional().isMobilePhone("any").withMessage("Invalid phone number"),
+  body("Country").optional().notEmpty().withMessage("Country is required"),
+];
+
+export const patchUserValidator = [
+  param("id").notEmpty().withMessage("UserID is required"),
+  body("FirstName").optional().notEmpty().withMessage("First Name cannot be empty"),
+  body("LastName").optional().notEmpty().withMessage("Last Name cannot be empty"),
+  body("Email").optional().isEmail().withMessage("Invalid email format"),
+  body("Phone").optional().isMobilePhone("any").withMessage("Invalid phone number"),
+  body("Country").optional().notEmpty().withMessage("Country cannot be empty"),
+];
+
+export const deleteUserValidator = [
+  param("id").notEmpty().withMessage("UserID is required"),
+];
